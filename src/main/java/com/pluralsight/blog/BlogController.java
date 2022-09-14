13cb2a7091a373c7044d94128bd60f717a9e3ce6
Amanda Ericson <amandaericson@hotmail.com>
2022-09-14 10:55:51 +0200

    Module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,27 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepo){
+        this.postRepository = postRepo;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelmap) {
-        modelmap.put("title", "Blog Post 1");
+        List<Post> posts = postRepository.getAllPosts();
+        modelmap.put("posts", posts);
         return "home";
     }
 }
